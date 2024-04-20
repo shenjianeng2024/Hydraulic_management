98d5e2b5fdf67c7995709f54b5a7bda22743c33c
shenjianeng2021 <shenjianeng2016@163.com>
2024-04-20 22:51:20 +0800

    feat: :sparkles: 简单页面的编写

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = App::new()?;
-    let ui_handle = ui.as_weak();
+    let _uihandle = ui.as_weak();
     ui.run()
 }
